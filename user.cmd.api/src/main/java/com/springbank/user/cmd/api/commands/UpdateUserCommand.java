5c7441f1610b0c52f48231f56c53714416c7fc85
michail.almyros <m.almyros@gmail.com>
2025-05-29 21:01:50 +0100

    - Adding initial models and commands

---
+++ user.cmd.api/src/main/java/com/springbank/user/cmd/api/commands/UpdateUserCommand.java
@@ -0,0 +1,16 @@
+package com.springbank.user.cmd.api.commands;
+
+import lombok.Builder;
+import lombok.Data;
+import models.User;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+public class UpdateUserCommand {
+
+    @TargetAggregateIdentifier
+    private String id;
+
+    private User user;
+}
---
+++ user.core/src/main/java/models/User.java
@@ -0,0 +1,27 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collation = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private Account account;
+}
