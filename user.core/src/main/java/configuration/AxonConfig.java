ebf21abb76394cff0d3301e21e726940e29edbc3
michail.almyros <m.almyros@gmail.com>
2025-05-29 16:01:06 +0100

    - Fixed the docker compose and axon configuration

--- user.core/src/main/java/configuration/AxonConfig.java
+++ user.core/src/main/java/configuration/AxonConfig.java
@@ -1,7 +1,10 @@
 package configuration;
 
+import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.thoughtworks.xstream.XStream;
 import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.eventhandling.tokenstore.TokenStore;
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
@@ -9,16 +12,17 @@ import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.extensions.mongo.DefaultMongoTemplate;
 import org.axonframework.extensions.mongo.MongoTemplate;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
-import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoFactory;
-import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoSettingsFactory;
 import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
 import org.axonframework.monitoring.MessageMonitor;
 import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.xml.XStreamSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class AxonConfig {
@@ -34,37 +38,50 @@ public class AxonConfig {
 
     @Bean
     public MongoClient mongo() {
-
-        MongoSettingsFactory mongoSettingsFactory = new MongoSettingsFactory();
         ServerAddress serverAddress = new ServerAddress(mongoHost, mongoPort);
-        mongoSettingsFactory.setMongoAddresses(Collections.singletonList(serverAddress));
-
-        MongoFactory mongoFactory = new MongoFactory();
-        mongoFactory.setMongoClientSettings(mongoSettingsFactory.createMongoClientSettings());
-        return mongoFactory.createMongo();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyToClusterSettings(builder ->
+                        builder.hosts(Collections.singletonList(serverAddress)))
+                .applyToSocketSettings(builder ->
+                        builder.connectTimeout(10, TimeUnit.SECONDS))
+                .build();
+        return MongoClients.create(settings);
     }
 
     @Bean
-    public MongoTemplate axonMongoTemplate() {
+    public MongoTemplate axonMongoTemplate(MongoClient mongoClient) {
         return DefaultMongoTemplate.builder()
-                .mongoDatabase(mongo(), mongoDatabase)
+                .mongoDatabase(mongoClient, mongoDatabase)
                 .build();
     }
 
     @Bean
-    public TokenStore tokenStore(Serializer serializer) {
+    @Primary
+    public Serializer serializer() {
+        XStream xStream = new XStream();
+        xStream.allowTypesByWildcard(new String[]{
+                "com.springbank.**",
+                "org.axonframework.**"
+        });
+        return XStreamSerializer.builder()
+                .xStream(xStream)
+                .build();
+    }
+
+    @Bean
+    public TokenStore tokenStore(MongoTemplate axonMongoTemplate, Serializer serializer) {
         return MongoTokenStore.builder()
-                .mongoTemplate(axonMongoTemplate())
+                .mongoTemplate(axonMongoTemplate)
                 .serializer(serializer)
                 .build();
     }
 
     @Bean
-    public EventStorageEngine storageEngine(MongoClient mongoClient) {
+    public EventStorageEngine storageEngine(MongoTemplate axonMongoTemplate, Serializer serializer) {
         return MongoEventStorageEngine.builder()
-                .mongoTemplate(DefaultMongoTemplate.builder()
-                        .mongoDatabase(mongoClient)
-                        .build())
+                .mongoTemplate(axonMongoTemplate)
+                .snapshotSerializer(serializer)       // important for snapshots!
+                .eventSerializer(serializer)          // important for events!
                 .build();
     }
 
